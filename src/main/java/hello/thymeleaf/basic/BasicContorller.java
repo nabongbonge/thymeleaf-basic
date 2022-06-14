1d5babc4a359486420070c95c8089a989a7a9ec2
jaeseugi <jaeseugi.1927@gmail.com>
2022-06-14 16:57:48 +0900

    메소드명 수정

--- src/main/java/hello/thymeleaf/basic/BasicContorller.java
+++ src/main/java/hello/thymeleaf/basic/BasicContorller.java
@@ -123,7 +123,7 @@ public class BasicContorller {
     }
 
     @GetMapping("/javascript")
-    public String script(Model model) {
+    public String javascript(Model model) {
         model.addAttribute("user", new User("UserA", 10));
         addUsers(model);
         return "basic/javascript";

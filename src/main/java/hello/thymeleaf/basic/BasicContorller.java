1ae8b9a8e1806f9d405d829d74912bd7e12d094b
jaeseugi <jaeseugi.1927@gmail.com>
2022-06-14 16:36:26 +0900

    반복, 조건부, 주석, 플록

--- src/main/java/hello/thymeleaf/basic/BasicContorller.java
+++ src/main/java/hello/thymeleaf/basic/BasicContorller.java
@@ -99,6 +99,38 @@ public class BasicContorller {
         return "basic/attribute";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comment(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+
+        model.addAttribute("users", list);
+    }
+
 
     @Component("helloBean")
     static class HelloBean {
